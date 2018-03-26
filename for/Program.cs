45e435f687797c0831ba5e202d463d68855691df
Makoto Tanabe <xekid78@outlook.com>
2018-03-26 11:06:08 +0900

    Add files via upload

---
+++ for/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace For
+{
+    class For
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 12; i++)
+            {
+                Console.WriteLine(i + "月");
+            }
+
+            // 実行結果確認のための処理 ここから
+            Console.WriteLine();
+            Console.WriteLine("何かキーを押してください");
+            Console.ReadKey();
+            // ここまで
+        }   
+    }   
+}
