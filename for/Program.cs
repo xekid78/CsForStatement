d1202b782ca4fd50089bc05cc8f5008ae3d47647
Makoto Tanabe <xekid78@outlook.com>
2018-03-27 14:42:14 +0900

    Add files via upload

--- for/Program.cs
+++ for/Program.cs
@@ -10,12 +10,6 @@ namespace For
             {
                 Console.WriteLine(i + "月");
             }
-
-            // 実行結果確認のための処理 ここから
-            Console.WriteLine();
-            Console.WriteLine("何かキーを押してください");
-            Console.ReadKey();
-            // ここまで
         }   
     }   
 }
